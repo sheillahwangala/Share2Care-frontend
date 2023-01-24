b6e324e47b029f9b7e050e20a9c16f196910d0f5
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2023-01-24 12:07:04 +0300

    add navlinks

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { NavLink } from "react-router-dom";
+import { IoIosBulb } from "react-icons/io";
 import "./navbar.css"
 
 
 function Navbar() {
 
     return (
-        <nav className="navbar navbar-expand-sm navbar-dark bg-info px-5">
-            <NavLink to='/' className="navbar-brand" >BRAVEBOOKS</NavLink>
+        <nav className="navbar navbar-expand-sm navbar-dark bg-black px-5">
+            <NavLink to='/' className="navbar-brand" > Share2Care <IoIosBulb />  </NavLink>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
@@ -17,7 +18,10 @@ function Navbar() {
                         <NavLink to='/' className="nav-link">Home <span className="sr-only"></span></NavLink>
                     </li>
                     <li className="nav-item">
-                        <NavLink to='/books' className="nav-link" >Add Books</NavLink>
+                        <NavLink to='/farmproduce' className="nav-link" >Farm Produce</NavLink>
+                    </li>
+                    <li className="nav-item">
+                        <NavLink to='/donate' className="nav-link" >Donate</NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink to='/signin' className="nav-link">Sign In</NavLink>
